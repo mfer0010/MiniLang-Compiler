720fafe4cbac132dc9a52e0ddea6004d03f0460a
mfer0010 <marc.ferriggi.15@um.edu.mt>
2018-03-27 16:45:27 +0200

    added File Structure

---
+++ cmake-build-debug/CMakeFiles/MiniLang_Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiniLang_Compiler.dir/main.cpp.o"
+  "MiniLang_Compiler.pdb"
+  "MiniLang_Compiler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiniLang_Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
