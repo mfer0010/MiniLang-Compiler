2159a8868ea318a0b226112e40ebf355560a2ee8
mfer0010 <marc.ferriggi.15@um.edu.mt>
2018-04-25 20:42:03 +0200

    Rmoved tutorial stuff and added assignment specific functions for parser

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "include/Parser.h"
-#include "include/Exceptions/MiniLangExceptions.h"
 #include "include/Exceptions/ParserException.h"
 
 
@@ -12,91 +11,45 @@ Parser::Parser(Lexer * lexer) {
 }
 
 void Parser::parse() {
-    ASTBinaryExprNode *Node;
-    try {
-        Node = parseBinaryExpression();
-    } catch(MiniLangExceptions &e) {
-        std::cout << e.printMessage() << std::endl;
-        throw -1;
-    }
-    //std::cout << Node->LHS->value << Node->_opertor << Node->RHS->value;
+//    ASTBinaryExprNode *Node;
+//    try {
+//        Node = parseBinaryExpression();
+//    } catch(MiniLangExceptions &e) {
+//        std::cout << e.printMessage() << std::endl;
+//        throw -1;
+//    }
 }
 
-//ASTExpression_Node* Parser::parseExpression() {
-//    TOKEN acceptedTokens [] = {TOK_Number, TOK_MultiplicativeOp, TOK_RelationalOp,TOK_AdditiveOp,TOK_Identifier};
-//    TOKEN *found = acceptedTokens;
-//
-//    ASTExpression_Node *toReturn;
-//
-//    //loop until the next token is not in acceptedTokens:
-//    while (found != std::end(acceptedTokens)) {
-//        currentToken = lexer->nextToken();
-//        switch (currentToken.token_name) {
-//            case TOK_Number:
-//                try {
-////                    ASTBinaryExprNode * = parseBinaryExpression();
-////                    toReturn.
-//                } catch (ParserException &e) {
-//                    throw e;
-//                }
-//            case TOK_MultiplicativeOp:
-//            case TOK_AdditiveOp:
-//            case TOK_RelationalOp:
-//            case TOK_Identifier:
-//        }
-//
-//        found = std::find(acceptedTokens, std::end(acceptedTokens),lexer->oneTokenLookahead().token_name);
+
+
+
+//ASTNumberExprNode* Parser::parseUnaryExpression() {
+//    switch(currentToken.token_name) {
+//        case TOK_Number:
+//            return new ASTNumberExprNode(currentToken.numerical_value, currentToken.string_value);
+//        default:
+//            throw ParserException("Expected Unary Expression",lexer->getErrorLine());
 //    }
 //}
-
-ASTExpression_Node *Parser::parseExpression() {
-
-    //First we parse the binary expression
-    ASTBinaryExprNode *binaryExpression = parseBinaryExpression();
-
-    //then we see if there's more to parse
-    TOKEN lookahead = lexer->oneTokenLookahead().token_name;
-    bool loop = lookahead == TOK_MultiplicativeOp || lookahead == TOK_AdditiveOp;
-    while(loop) {
-        //if multiplicative op, do that first
-        if (lookahead == TOK_MultiplicativeOp) {
-
-        } else {
-            //else do it after
-        }
-
-        lookahead = lexer->oneTokenLookahead().token_name;
-        loop = lookahead == TOK_MultiplicativeOp || lookahead == TOK_AdditiveOp;
-    }
-}
-
-ASTNumberExprNode* Parser::parseUnaryExpression() {
-    switch(currentToken.token_name) {
-        case TOK_Number:
-            return new ASTNumberExprNode(currentToken.numerical_value, currentToken.string_value);
-        default:
-            throw ParserException("Expected Unary Expression",lexer->getErrorLine());
-    }
-}
-
-ASTBinaryExprNode* Parser::parseBinaryExpression() {
-    currentToken = lexer->nextToken();
-
-    ASTNumberExprNode *LHS;
-    ASTNumberExprNode *RHS;
-    std::string _operator;
-
-    TOKEN opToken = lexer->oneTokenLookahead().token_name;
-    if (opToken == TOK_MultiplicativeOp || opToken == TOK_AdditiveOp) {
-        LHS = parseUnaryExpression();
-
-        currentToken = lexer->nextToken();
-        _operator = currentToken.string_value;
-
-        currentToken = lexer->nextToken();
-        RHS = parseUnaryExpression();
-
-        return new ASTBinaryExprNode(_operator, LHS, RHS);
-    }
-    throw ParserException("Expected Binary Expression", lexer->getErrorLine());
-}
+//
+//ASTBinaryExprNode* Parser::parseBinaryExpression() {
+//    currentToken = lexer->nextToken();
+//
+//    ASTNumberExprNode *LHS;
+//    ASTNumberExprNode *RHS;
+//    std::string _operator;
+//
+//    TOKEN opToken = lexer->oneTokenLookahead().token_name;
+//    if (opToken == TOK_MultiplicativeOp || opToken == TOK_AdditiveOp) {
+//        LHS = parseUnaryExpression();
+//
+//        currentToken = lexer->nextToken();
+//        _operator = currentToken.string_value;
+//
+//        currentToken = lexer->nextToken();
+//        RHS = parseUnaryExpression();
+//
+//        return new ASTBinaryExprNode(_operator, LHS, RHS);
+//    }
+//    throw ParserException("Expected Binary Expression", lexer->getErrorLine());
+//}
--- src/include/Parser.h
+++ src/include/Parser.h
@@ -9,6 +9,15 @@
 #include "ASTNodes/ASTExpression_Node.h"
 #include "ASTNodes/ASTNumberExprNode.h"
 #include "ASTNodes/ASTBinaryExprNode.h"
+#include "ASTNodes/ASTStatement_Node.h"
+#include "ASTNodes/ASTVariableDeclStmtNode.h"
+#include "ASTNodes/ASTAssignmentStmtNode.h"
+#include "ASTNodes/ASTPrintStmtNode.h"
+#include "ASTNodes/ASTIfStmtNode.h"
+#include "ASTNodes/ASTWhileStmtNode.h"
+#include "ASTNodes/ASTReturnStmtNode.h"
+#include "ASTNodes/ASTFuncDeclStmtNode.h"
+#include "ASTNodes/ASTBlockStmtNode.h"
 
 
 class Parser {
@@ -23,14 +32,70 @@ private:
 
     Token currentToken = Token(TOK_Skip);
 
+    /*
+     * Function to Parse a statement.
+     * According to EBNF:
+     * <Statement> ::= <VariableDecl>;
+     *              | <Assignment>;
+     *              | <PrintStatement>;
+     *              | <IfStatement>
+     *              | <WhileStatement>
+     *              | <ReturnStatement>;
+     *              | <FunctionDecl>
+     *              | <Block>
+     */
+    ASTStatement_Node *parseStatement();
 
-    /* Uses ParseUnaryExpression and Parse Binary Expression to
-     * parse any expression and take care of operator precedence */
-    ASTExpression_Node * parseExpression();
+    //<VariableDecl> ::= 'var' <Identifier> '=' <Expression>
+    ASTVariableDeclStmtNode *parseVariableDecl();
 
-    ASTNumberExprNode * parseUnaryExpression();
+    //<Assignment> ::= 'set' <Identifier> '=' <Expression>
+    ASTAssignmentStmtNode *parseAssignment();
 
-    ASTBinaryExprNode * parseBinaryExpression();
+    //<PrintStatement> ::= 'print' ,Expression
+    ASTPrintStmtNode *parsePrintStatement();
+
+    //<IfStatement> ::= 'if' '(' <Expression> ')' <Block> ['else' <Block>]
+    ASTIfStmtNode *parseIfStatement();
+
+    //<WhileStatement> ::= 'while' '(' <Expression> ')' <Block>
+    ASTWhileStmtNode *parseWhileStatement();
+
+    //<ReturnStatement> ::= 'return' <Expression>
+    ASTReturnStmtNode *parseReturnStatement();
+
+    //<FunctionDecl> ::= 'def' <Identifier> '('[<FormalParams>]')' ':' <Type> <Block>
+    ASTFuncDeclStmtNode *parseFunctionDeclStatement();
+
+    //<Block> ::= '{' {<Statement>} '}'
+    ASTBlockStmtNode *parseBlock();
+
+    //<Expression> ::= <SimpleExpression> {<RelationalOp> <SimpleExpression>}
+    ASTExpression_Node *parseExpression();
+
+    //<FormalParams> ::= <FormalParam> {',' <FormalParam>}
+    std::vector<ASTFormalParamStmtNode *> parseFormalParams();
+
+    //<FormalParam> ::= <Identifier> ':' <Type>
+    ASTFormalParamStmtNode *parseFormalParam();
+
+    //<SimpleExpression> ::= <Term> {<AdditiveOp> <Term>}
+    ASTExpression_Node *parseSimpleExpression();
+
+    //<Term> ::= <Factor> {<MultiplicativeOp> <Factor>}
+    ASTExpression_Node *parseTerm();
+
+    /* <Factor> ::= <Literal>
+     *          | <Identifier>
+     *          | <FunctionCall>
+     *          | <SubExpression>
+     *          | <Unary>
+     */
+    ASTExpression_Node *parseFactor();
+
+//    ASTNumberExprNode *parseUnaryExpression();
+//
+//    ASTBinaryExprNode *parseBinaryExpression();
 };
 
 
